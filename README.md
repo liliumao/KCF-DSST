e0018c87cc5ff1537a87acc8465d92f4e075ad4e
liliumao <liliumao@gmail.com>
2016-06-07 16:42:14 +0800

    readme modified

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # KCF-DSST
+
+This package includes a C++ class with several tracking methods based on the Kernelized Correlation Filter (KCF) [1, 2] for translation changes and the Discriminative Scale Space Tracker (DSST) [3] .   
+
+DSST scaling changing part is added to the original kcftracker.cpp file. Original recttools.cpp and ffttools.cpp are also modified. Debug mode is added to the Cmakelists.txt.
+
+[1] J. F. Henriques, R. Caseiro, P. Martins, J. Batista,   
+"High-Speed Tracking with Kernelized Correlation Filters", TPAMI 2015.
+
+[2] J. F. Henriques, R. Caseiro, P. Martins, J. Batista,   
+"Exploiting the Circulant Structure of Tracking-by-detection with Kernels", ECCV 2012.
+
+[3] M. Danelljan, G. Häger, F. Shahbaz Khan, and M. Felsberg. Accurate scale estimation for robust visual tracking. In Proceedings of the British Machine Vision Conference (BMVC), 2014.
--- src/runtracker.cpp
+++ src/runtracker.cpp
@@ -1,18 +1,50 @@
 #include <iostream>
+#include <stdio.h>
 #include <fstream>
 #include <sstream>
 #include <algorithm>
-
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include "opencv2/imgproc/imgproc.hpp"
 
 #include "kcftracker.hpp"
 
 #include <dirent.h>
-#include <time.h>
 
 using namespace std;
 using namespace cv;
+
+// Frame readed
+Mat frame;
+// Tracker results
+Rect result;
+bool select_flag=false;
+bool selected = false;
+Point origin;
+
+void onMouse(int event,int x,int y,int flags, void* userdata)
+{
+    //Point origin;//不能在这个地方进行定义，因为这是基于消息响应的函数，执行完后origin就释放了，所以达不到效果。
+    if(select_flag)
+    {
+        result.x=MIN(origin.x,x);//不一定要等鼠标弹起才计算矩形框，而应该在鼠标按下开始到弹起这段时间实时计算所选矩形框
+        result.y=MIN(origin.y,y);
+        result.width=abs(x-origin.x);//算矩形宽度和高度
+        result.height=abs(y-origin.y);
+        result&=Rect(0,0,frame.cols,frame.rows);//保证所选矩形框在视频显示区域之内
+    }
+    if(event==EVENT_LBUTTONDOWN)
+    {
+        select_flag=true;//鼠标按下的标志赋真值
+        origin=Point(x,y);//保存下来单击是捕捉到的点
+        result=Rect(x,y,0,0);//这里一定要初始化，宽和高为(0,0)是因为在opencv中Rect矩形框类内的点是包含左上角那个点的，但是不含右下角那个点
+    }
+    else if(event==EVENT_LBUTTONUP)
+    {
+        select_flag=false;
+        selected = true;
+    }
+}
 
 int main(int argc, char* argv[]){
 
@@ -23,6 +55,7 @@ int main(int argc, char* argv[]){
 	bool MULTISCALE = true;
 	bool SILENT = false;
 	bool LAB = false;
+	VideoCapture cam(0); //webcam
 
 	for(int i = 0; i < argc; i++){
 		if ( strcmp (argv[i], "hog") == 0 )
@@ -44,98 +77,78 @@ int main(int argc, char* argv[]){
 	// Create KCFTracker object
 	KCFTracker tracker(HOG, FIXEDWINDOW, MULTISCALE, LAB);
 
-	// Frame readed
-	Mat frame;
 
-	// Tracker results
-	Rect result;
 
-	string base = "Test/CarDark/";
+	//New window
+	string window_name = "video | q or esc to quit";
+	namedWindow(window_name, 1);
 
-	// Path to list.txt
-	ifstream listFile;
-	string fileName = base + "images.txt";
-  	listFile.open(fileName);
-
-  	// Read groundtruth for the 1st frame
-  	ifstream groundtruthFile;
-	string groundtruth = base + "init.txt";
-  	groundtruthFile.open(groundtruth);
-  	string firstLine;
-  	getline(groundtruthFile, firstLine);
-	groundtruthFile.close();
-
-  	istringstream ss(firstLine);
-		printf("%s\n", groundtruth.c_str());
-
-  	// Read groundtruth like a dumb
-  	// float x1, y1, x2, y2;
-
-	float xMin;
-	float yMin;
-	float width;
-	float height;
-	ss >> xMin;
-	ss >> yMin;
-	ss >> width;
-	ss >> height;
+	//set Rect
+	// setMouseCallback(window_name,onMouse,0);
 
 	// Using min and max of X and Y for groundtruth rectangle
-
-
-	// printf("%f:%f:%f:%f\n", width, height, xMin, yMin);
-	// Read Images
-	ifstream listFramesFile;
-	string listFrames = base + "images.txt";
-	listFramesFile.open(listFrames);
-	string frameName;
-
+	// float xMin =  result.x;
+	// float yMin =  result.y;
+	// float width = result.width;
+	// float height = result.height;
 
 	// Write Results
-	// ofstream resultsFile;
-	// string resultsPath = "output.txt";
-	// resultsFile.open(resultsPath);
+	ofstream resultsFile;
+	string resultsPath = "output.txt";
+	resultsFile.open(resultsPath);
 
 	// Frame counter
 	int nFrames = 0;
 
-	clock_t start,end; // typedef long clock_t
-	start = clock();
+	while ( 1 ){
+		cam >> frame;
 
-	while ( getline(listFramesFile, frameName) ){
-		frameName = frameName;
+    float xMin =  result.x;
+  	float yMin =  result.y;
+  	float width = result.width;
+  	float height = result.height;
+    setMouseCallback(window_name,onMouse,0);
 
-		// Read each frame from the list
-		frame = imread(base + frameName, CV_LOAD_IMAGE_COLOR);
+    if (selected){
+      // First frame, give the groundtruth to the tracker
+      if (nFrames == 0) {
+        tracker.init( Rect(xMin, yMin, width, height), frame );
+        // rectangle( frame, Point( xMin, yMin ), Point( xMin+width, yMin+height), Scalar( 0, 255, 255 ), 1, 8 );
+        // resultsFile << xMin << "," << yMin << "," << width << "," << height << endl;
+      }
+    // 	// Update
+      else{
+    		result = tracker.update(frame);
+        rectangle( frame, Point( result.x, result.y ), Point( result.x+result.width, result.y+result.height), Scalar( 0, 255, 255 ), 1, 8 );
+    // 		resultsFile << result.x << "," << result.y << "," << result.width << "," << result.height << endl;
+      }
+      // printf("%s\n", "a");
+      nFrames++;
+    }else{
+      rectangle( frame, Point( xMin, yMin ), Point( xMin+width, yMin+height), Scalar( 0, 255, 255 ), 1, 8 );
+    }
 
-		// First frame, give the groundtruth to the tracker
-		if (nFrames == 0) {
-			tracker.init( Rect(xMin, yMin, width, height), frame );
-			rectangle( frame, Point( xMin, yMin ), Point( xMin+width, yMin+height), Scalar( 0, 255, 255 ), 1, 8 );
-			// resultsFile << xMin << "," << yMin << "," << width << "," << height << endl;
-		}
-		// Update
-		else{
-			result = tracker.update(frame);
-			rectangle( frame, Point( result.x, result.y ), Point( result.x+result.width, result.y+result.height), Scalar( 0, 255, 255 ), 1, 8 );
-			// resultsFile << result.x << "," << result.y << "," << result.width << "," << result.height << endl;
-		}
-
-		nFrames++;
 
 		if (!SILENT){
-			imshow("Image", frame);
-			waitKey(1);
+			imshow(window_name, frame);
 		}
+
+		char key = (char)waitKey(30); //delay N millis, usually long enough to display and capture input
+
+    switch (key) {
+    case 'q':
+    case 'Q':
+    case 27: //escape key
+        return 0;
+		default:
+        break;
+    }
+    // if(key == 27)
+    //   break;
+
 	}
-
-	end = clock();
-
-	double duration =(double)(end-start)/CLOCKS_PER_SEC;
-  printf("%f\n",nFrames/duration);
-
 	// resultsFile.close();
 
-	listFile.close();
+  return 0;
 
 }
